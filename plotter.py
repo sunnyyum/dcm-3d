9d14b75bbd199f1e77a717ae238a49b9bdfb09cd
KYU SEON YUM <rbtjs0110@gmail.com>
2020-07-11 05:20:05 -0700

    Fix the argparser part

--- plotter.py
+++ plotter.py
@@ -1,17 +1,7 @@
 import argparse
 import sys
-import os.path as path
-from graphic_handler.graphic_util import *
 
-
-def str_to_bool(value):
-    if isinstance(value, bool):
-        return value
-    if value.lower() in {'false', 'f', '0', 'no', 'n'}:
-        return False
-    elif value.lower() in {'true', 't', '1', 'yes', 'y'}:
-        return True
-    raise ValueError(f'{value} is not a valid boolean value')
+from graphic_handler.graphic_util import show_obj
 
 
 def read_opt(args: list) -> argparse.Namespace:
@@ -28,30 +18,24 @@ def read_opt(args: list) -> argparse.Namespace:
     argparse.Namespace
          an object holding attribute of input numpy paths
     """
-    parse = argparse.ArgumentParser(description="visualize 3D object from the input object")
+    parser = argparse.ArgumentParser(description="visualize 3D object from the input object")
 
-    parse.add_argument('-i', required=True, dest='input_obj',
-                       help='input 3D object file')
+    parser.add_argument('-i', required=True, dest='input_obj', help='input 3D object file')
 
-    parse.add_argument('--ani', required=False, default=False, dest='animation', type=str_to_bool, nargs='?',
-                       const=True, help='orbiting the object')
+    ani_group = parser.add_mutually_exclusive_group()
+    ani_group.add_argument('--no-ani', default=False, dest='animation', action='store_false',
+                           help='only display the input object')
 
-    parse.add_argument('-o', required=False, dest='output_path',
-                       help='output file including file path')
+    ani_group.add_argument('--ani', dest='output_path', help='save the orbiting object to output file')
 
-    return parse.parse_args(args)
+    return parser.parse_args(args)
 
 
 if __name__ == '__main__':
     opts = read_opt(sys.argv[1::])
 
     file_path = opts.input_obj
-    output_path = opts.output_path if not opts.output_path else 'None'
-    is_ani = opts.animation
-
-    if is_ani and output_path:
-        _, ext = path.splitext(output_path)
-        if ext != '.gif':
-            assert f'{output_path} is not valid extension for gif'
+    output_path = opts.output_path
+    is_ani = opts.animation if not output_path else True
 
     show_obj(file_path, turning=is_ani, output_path=output_path)
